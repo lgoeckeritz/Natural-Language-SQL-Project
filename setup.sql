c19fc2a2d77c0786b26b3fed317f7e9f02ef9efb
Layton Goeckeritz <laygoeckeritz@gmail.com>
2024-02-01 18:58:56 -0700

    uploading setup

---
+++ setup.sql
@@ -0,0 +1,44 @@
+create table photo (
+	ID int primary key auto_increment,
+    dateAndTime datetime,
+    locationID int,
+    deviceType varchar(20),
+    dataType varchar(6) not null,
+    foreign key (locationID) references location (ID)
+);
+
+create table location (
+    ID int primary key auto_increment,
+    longitude decimal(10,8) not null,
+    latitude decimal(11,8) not null
+);
+
+create table person (
+    ID int primary key auto_increment,
+    firstName varchar(20) not null,
+    lastName varchar(20) not null
+);
+
+create table personInPhoto (
+    personID int,
+    photoID int,
+    foreign key (personID) references person (ID),
+    foreign key (photoID) references photo (ID),
+    primary key (personID, photoID)
+);
+
+create table album (
+    ID int primary key auto_increment,
+    albumName varchar(20) not null unique
+);
+
+create table photoInAlbum (
+    photoID int,
+    albumID int,
+    foreign key (photoID) references photo (ID),
+    foreign key (albumID) references album (ID),
+    primary key (photoID, albumID)
+);
+
+
+
